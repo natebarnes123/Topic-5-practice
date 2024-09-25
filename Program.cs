79627fe19a3b4f151e3bde24e53595b3a4883419
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-25 09:37:06 -0400

    started topic 5

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
+            string sigma;
+            Console.WriteLine("What is the word for the alpha wolf?");
+            sigma = Console.ReadLine();
+            if (sigma == "correct!") ;
             
-
-
-
-
 
 
 
